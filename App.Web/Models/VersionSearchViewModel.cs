936889905a131fcbe2836b5dbc0689c0ba7caad5
Simone Spreafico <simo.sprea1907@gmail.com>
2020-10-16 16:52:19 +0200

    frontend regex validation & little mod in index

--- App.Web/Models/VersionSearchViewModel.cs
+++ App.Web/Models/VersionSearchViewModel.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Web.Models
 {
     public class VersionSearchViewModel
     {        
+        [RegularExpression(@"(\d+)\.(\d+)\.(\d+)|(\d+)\.(\d+)",
+            ErrorMessage = "Not a valid version")]
+        [MaxLength(5)]
         public string SearchTerm { get; set; }
 
         public List<Software> SearchResults { get; set; }
